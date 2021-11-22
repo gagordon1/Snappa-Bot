cb340fd2bca107f27f381666b3f5ab5659ada5be
Garrett Gordon <gagordon@mit.edu>
2021-11-22 18:38:42 -0500

    leaderboard spec

---
+++ SnappaLeaderboard.py
@@ -0,0 +1,60 @@
+
+
+class SnappaLeaderboard:
+
+
+
+    def get_leaderboard(self, n):
+        """Get the top n players in the leaderboard along with their overall
+        record
+
+        Parameters
+        ----------
+        n : int
+            Number of players to return
+
+        Returns
+        -------
+        String
+            The top n players in the leaderboard along with their record
+
+        """
+
+
+    def add_player(self, name):
+        """Add a player to the leaderboard system
+
+        Parameters
+        ----------
+        name : String
+            name of the user
+
+        Returns
+        -------
+        Boolean
+            True if player successfully added, false otherwise
+
+        """
+
+
+
+    def log_score(self):
+        """Log a game to the system
+
+        Returns
+        -------
+        Boolean
+            True if successfully logged, false otherwise
+
+        """
+
+
+    def generate_message(self):
+        """Generates a randomly chosen message
+
+        Returns
+        -------
+        String
+            Randomly chosen message from a set of phrases
+
+        """
