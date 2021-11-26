f9048418d28795c9cf94b1657b0dc17a1819ea99
Garrett Gordon <gagordon@mit.edu>
2021-11-26 15:12:43 -0500

    implemented naive dictionary database

--- DictionaryDatabase.py
+++ DictionaryDatabase.py
@@ -5,7 +5,12 @@ class DictionaryDatabase:
         self.players = {}
 
     def get_player_games(self, name : str):
-        pass
+        out = []
+        for game in self.games:
+            if name in game[1:5]:
+                out.append(game[:])
+        return out
+
 
     def get_player_data(self, player : str):
         if player not in self.players:
@@ -15,7 +20,11 @@ class DictionaryDatabase:
 
     def updatePlayerData(self, player : str, ELO : int,
         number_of_wins : int, number_of_losses : int):
-        pass
+        if player not in self.players:
+            return "Player {} does not exist in the database!".format(player)
+        else:
+            self.players[player] = [ELO, number_of_wins, number_of_losses]
+            return "Player data successfully updated!"
 
     def log_game(self, timestamp : int,
             player1 : str,
@@ -24,15 +33,21 @@ class DictionaryDatabase:
             player4 : str,
             team_1_score : int,
             team_2_score : int):
-        pass
+        p = [player1, player2, player3, player4]
+        for player in p:
+            if player not in self.players:
+                return "Player {} does not exist in the database!".format(player)
+        self.games.append([timestamp, *p, team_1_score, team_2_score])
+        return "Game successfully logged!"
 
     def get_leaderboard(self):
-        pass
-
-
+        out = []
+        for player in self.players:
+            out.append([player, *self.players[player][:]])
+        return out
 
     def add_player(self, name : str, initial_elo :int, initial_wins : int, initial_losses : int):
-    
+
         if name in self.players:
             return "Player already exists in the database!"
         else:
