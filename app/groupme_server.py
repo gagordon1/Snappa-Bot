bab9d88f6989e0ea12a9fcfec61bccca5329a5fd
Garrett Gordon <gagordon@mit.edu>
2021-11-26 20:25:26 -0500

    testing server

--- app/groupme_server.py
+++ app/groupme_server.py
@@ -9,7 +9,12 @@ app = Flask(__name__)
 
 @ app.route("/", methods=["GET"])
 def home():
-    return requst.text
+    if request.method == "GET":
+        return "WELCOME TO SNAPPA BOT BITCH"
+    elif request.method == "POST":
+        return request.data
+    else:
+        return "WELCOME TO SNAPPA BOT"
 
 if __name__ == "__main__":
     app.run(debug=True, port=PORT)
