07810e3fe0f7ae72c7efb2ebe6665b9c82d1bb1b
Garrett Gordon <gagordon@mit.edu>
2021-11-27 03:43:17 -0500

    testing groupme with mongo

--- groupme_server_test.py
+++ groupme_server_test.py
@@ -14,12 +14,12 @@ def send_groupme_request(text, name):
 
 if __name__ == '__main__':
 
-    # names = ["Garrett Gordon", "Andrei Dumitrescu", "Noah Faro", "Sebastian Simon"]
-    # for name in names:
-    #     text = "@SnappaBot /add @{}".format(name)
-    #     send_groupme_request(text, name)
+    names = ["Garrett Gordon", "Andrei Dumitrescu", "Noah Faro", "Sebastian Simon"]
+    for name in names:
+        text = "@SnappaBot /add @{}".format(name)
+        send_groupme_request(text, name)
 
-    # text = "@SnappaBot /score @{} @{} @{} @{}, 7-2".format(*names)
-    # text = "@SnappaBot /lb"
+    text = "@SnappaBot /score @{} @{} @{} @{}, 7-2".format(*names)
+    text = "@SnappaBot /lb"
     text = "@SnappaBot fuck u"
     send_groupme_request(text, "Garrett Gordon")
