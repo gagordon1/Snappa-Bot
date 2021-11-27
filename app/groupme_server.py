1262527708b922722edd08be5343635630d747e9
Garrett Gordon <gagordon@mit.edu>
2021-11-26 21:18:45 -0500

    moving endpoints to same flask file

--- app/groupme_server.py
+++ app/groupme_server.py
@@ -1,11 +1,36 @@
 #READ groupme import json
 from flask import Flask, request, redirect, g, render_template, jsonify
 import json
+import requests
+from Databases.DictionaryDatabase import DictionaryDatabase
+from Databases.GoogleSheetsDatabase import GoogleSheetsDatabase
+from SnappaLeaderboard import SnappaLeaderboard
+import sys
 
 
 PORT = 8080
 
 app = Flask(__name__)
+
+GROUPME_ID = 163797812604437644 #TEST GROUP
+
+"""
+#POST requests from groupme appear like:
+{
+    "attachments":[],
+    "avatar_url":"https://i.groupme.com/750x744.jpeg.0f268692d87a4254a35623c95efa73ba",
+    "created_at":1637978126,
+    "group_id":"83775365",
+    "id":"163797812604437644",
+    "name":"Garrett Gordon",
+    "sender_id":"38446275",
+    "sender_type":"user",
+    "source_guid":"C0227DA0-438D-4A39-ABF0-36EE77222B24",
+    "system":false,
+    "text":"hi",
+    "user_id":"38446275"
+}
+"""
 
 
 @ app.route("/", methods=["GET", "POST"])
@@ -14,14 +39,143 @@ def home():
     if request.method == "GET":
         return "WELCOME TO SNAPPA BOT BITCH"
     elif request.method == "POST":
-        data = request.data.decode("UTF-8")
-        print(type(data))
+        data = json.loads(request.data.decode("UTF-8"))
         text = data["text"]
         author = data["name"]
-
         return request.data
     else:
         return "WELCOME TO SNAPPA BOT"
+
+db = DictionaryDatabase()
+# db = GoogleSheetsDatabase(test = True)
+slb = SnappaLeaderboard(db)
+
+"""
+POST
+    json data
+    name : str
+    initial_elo : int
+    initial_wins : int
+    initial_losses : int
+
+    Response:
+    "Player {name} successfully added!" | "Player {name}'s name is too long"
+    | Player's name cannot be empty!
+
+GET
+    name : str
+
+    Response:
+    Tabulated string of player data | "Player data could not be accessed!"
+
+"""
+@ app.route("/players", methods=["POST", "GET"])
+def players():
+    response = ""
+    if request.method == "POST":
+        data = request.json
+        try:
+            slb.add_player(
+                data["name"],
+                data["initial_elo"],
+                data["initial_wins"],
+                data["initial_losses"]
+            )
+            return "Player {} successfully added!".format(data["name"])
+        except Exception as e:
+            return str(e)
+        return "Error adding player!"
+
+    elif request.method == "GET":
+        data = request.json
+        try:
+            response = slb.get_player_data(data["name"])
+            return response
+        except Exception as e:
+            return str(e)
+        return "Player data could not be accessed!"
+
+    else:
+        return "Only POST and GET requests are supported!"
+
+
+
+
+"""
+POST
+    json data
+    player1 : str
+    player2 : str
+    player3 : str
+    player4 : str
+    team_1_score : int
+    team_2_score : int
+
+    Response:
+    Table of posted update string | "Game could not be logged!"
+    | "Player {name} does not exist in the database!"
+    | Game successfully logged!
+"""
+@ app.route("/games", methods=["POST"])
+def games():
+    if request.method == "POST":
+        data = request.json
+        try:
+            response = slb.log_score(
+                data["player1"],
+                data["player2"],
+                data["player3"],
+                data["player4"],
+                data["team_1_score"],
+                data["team_2_score"],
+            )
+            return response
+        except Exception as e:
+            return str(e)
+
+        return "Game could not be logged!"
+    else:
+        return "Only POST requests are supported!"
+
+"""
+GET
+    json data
+    n: int
+
+    Response:
+    Leaderboard String | "Database could not be accessed!"
+"""
+@ app.route("/leaderboard", methods=["GET"])
+def leaderboard():
+    if request.method == "GET":
+        data = request.json
+        n = data["n"]
+        try:
+            response = slb.get_leaderboard(n)
+            return response
+        except Exception as e:
+            return str(e)
+        return "Database could not be accessed!"
+
+    else:
+        return "Only GET requests are supported!"
+
+"""
+GET
+    Response:
+    Random message
+"""
+@ app.route("/message", methods=["GET"])
+def message():
+    if request.method == "GET":
+        try:
+            response = slb.generate_message()
+            return response
+        except:
+            return "Message could not be generated!"
+
+    else:
+        return "Only GET requests are supported!"
 
 if __name__ == "__main__":
     app.run(debug=True, port=PORT)
--- snappa_leaderboard_server_test.py
+++ snappa_leaderboard_server_test.py
@@ -1,6 +1,6 @@
 import requests
 
-BASE_URL = "http://127.0.0.1:8080"
+BASE_URL = "https://snappa-bot-groupme-server.herokuapp.com/"
 
 INITIAL_ELO = 1500
 INITIAL_WINS = 0
