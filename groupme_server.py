6520c6087e3dcd0d203688aeb2715c5c5feeeb6b
Garrett Gordon <gagordon@mit.edu>
2021-11-26 19:37:50 -0500

    made groupme server file

---
+++ groupme_server.py
@@ -0,0 +1,15 @@
+#READ groupme import json
+from flask import Flask, request, redirect, g, render_template, jsonify
+
+
+PORT = 8080
+
+app = Flask(__name__)
+
+
+@ app.route("/", methods=["GET"])
+def home():
+    return "WELCOME TO SNAPPA BOT BITCH"
+
+if __name__ == "__main__":
+    app.run(debug=True, port=PORT)
