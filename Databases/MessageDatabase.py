bad187ef3de2c263f6a2bd63ec11c0020bc8b58f
Garrett Gordon <gagordon@mit.edu>
2022-09-12 23:58:04 -0400

    messages update

--- Databases/MessageDatabase.py
+++ Databases/MessageDatabase.py
@@ -1,8 +1,8 @@
 messages = [
-    "I'm snappa bot bitch",
+    "I'm snappa bot",
     "Have you met my grandfather, Professor Phillip D. Snutz?",
-    "Barry McCockiner",
-    "Fuck you",
+    "Gargladis",
+    "DKE Senior",
     "RIP Young Dolph",
     "Appear weak when you are strong, and strong when you are weak.",
     "If you know the enemy and know yourself, you need not fear the result of a hundred battles. If you know yourself but not the enemy, for every victory gained you will also suffer a defeat. If you know neither the enemy nor yourself, you will succumb in every battle.",
